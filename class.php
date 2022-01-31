944f1edf5891999489539a5751ebf43d09a0b4f4
mahekmk <mahekkalola4@gmail.com>
2022-01-31 13:05:35 +0530

    php class functions

--- class.php
+++ class.php
@@ -151,6 +151,7 @@ $test->test();
 echo '<br>';
 echo '10. get_parent_class';
 echo '<br>';
+
 class Dad {
     function __construct()
     {
@@ -173,5 +174,184 @@ class Child2 extends Dad {
 
 $foo4 = new child();
 $bar4= new child2();
+
+
+
+
+echo '<br>';
+echo '11. get_declared_traits';
+echo '<br>';
+
+//namespace Example;
+
+trait fooTraits
+{
+}
+
+abstract class fooAbstract
+{
+
+}
+
+class Bar5 extends fooAbstract
+{
+    use fooTraits;
+}
+
+$array = get_declared_traits();
+var_dump($array);
+
+
+echo '<br>';
+echo '12. interface_exists()';
+echo '<br>';
+
+if(interface_exists('MyInterface'))
+{
+    class MyClass implements MyInterface
+    {
+        //methods
+    }
+
+}
+
+echo '<br>';
+echo '13. is_a()';
+echo '<br>';
+
+class foo6 { 
+
+}
+$a = new foo6;
+
+if(is_a($a,'foo6'))
+    echo 'yes, a is object of foo6.';
+
+echo '<br>';
+echo '<br>';
+echo '14. is_subclass_of()';
+echo '<br>';
+
+class dadJohn{
+
+}
+
+class childMike extends dadJohn{
+}
+
+$dad = new dadJohn();
+$child = new childMike();
+
+
+if(is_subclass_of('childMike', 'dadJohn'))
+    echo 'yes child classMike is subclass of dadJohn class.';
+else 
+    echo 'no child classMike is not subclass of dadJohn class.';
+
+echo '<br>';
+echo '<br>';
+echo '15. method_exists()';
+echo '<br>';
+
+$directory = new Directory('.');
+var_dump(method_exists($directory,'read'));
+
+echo '<br>';
+echo '<br>';
+echo '16. property_exists()';
+echo '<br>';
+
+class myClass {
+    public $mine;
+    private $xpto;
+    static protected $test;
+
+    static function test() {
+        var_dump(property_exists('myClass', 'xpto')); //true
+    }
+}
+
+var_dump(property_exists('myClass', 'mine'));   //true
+var_dump(property_exists(new myClass, 'mine')); //true
+var_dump(property_exists('myClass', 'xpto'));   //true
+var_dump(property_exists('myClass', 'bar'));    //false
+var_dump(property_exists('myClass', 'test'));   //true
+myClass::test();
+
+
+echo '<br>';
+echo '<br>';
+echo '17. trait_exists()';
+echo '<br>';
+
+
+if(trait_exists('fooTraits'))
+    echo "footraits exists";
+
+echo '<br>';
+echo '<br>';
+echo '18. get_mangled_object_vars()';
+echo '<br>';
+
+class A
+{
+    public $public =1;
+    protected $protected =2;
+    private $private = 3;
+
+}
+
+class B extends A
+{
+    private $private = 4;
+}
+$object = new B;
+$object->dynamic = 5;
+$object->{'6'} = 6;
+
+var_dump(get_mangled_object_vars($object));
+
+class AO extends ArrayObject
+{
+    private $private = 1;
+}
+
+$arrayObject = new AO(['x' => 'y']);
+$arrayObject->dynamic = 2;
+
+var_dump(get_mangled_object_vars($arrayObject));
+
+
+echo '<br>';
+echo '<br>';
+echo '19. enum_exists';
+echo '<br>';
+
+enum checkGender:string{
+    case MALE = 'male';
+    case Female = 'female';
+}
+
+
+if (enum_exists(checkGender::class)) {
+  echo "Valid ";
+} else {
+  echo "not valid";
+}
+
+
+echo '<br>';
+echo '<br>';
+echo '20. function_exists';
+echo '<br>';
+
+
+if (function_exists('myfunc11')) {
+    echo "myfunc11 functions are available.<br />\n";
+} else {
+    echo "myfunc11 functions are not available.<br />\n";
+}
+
+
 
 ?>
